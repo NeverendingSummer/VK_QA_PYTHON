3aec8bc150b66729ce84ad0c09ffb3c775a5aeb1
ayypeegeepee <blessrng17@gmail.com>
2022-10-17 15:50:22 +0300

    new commit for hm2 17_10_22

--- homework2/test_hm1.py
+++ homework2/test_hm1.py
@@ -7,7 +7,7 @@ import random
 class TestOne(BaseCase):
 
     # Тест открытия окна авторизации с последующим вводом корректных данных
-    #@pytest.mark.skip('skip')
+    @pytest.mark.skip('skip')
     def test_login(self, driver):
         self.base_page.login("blessrng17@gmail.com", "testpass123")
 
@@ -19,21 +19,21 @@ class TestOne(BaseCase):
         self.base_page.authorization(email, password)
 
     # Тест выхода из профиля
-    @pytest.mark.skip('skip')
+    #@pytest.mark.skip('skip')
     def test_logout(self):
         self.base_page.login("blessrng17@gmail.com", "testpass123")
         self.base_page.logout()
 
-    # Параметризованный тест на переход по навигационному полю. Правда из пула убран PRO, потому что он по кд ломается
-    @pytest.mark.skip('skip')
-    @pytest.mark.parametrize("nav", [(random.randint(0, 4)),
-                                     (random.randint(0, 4))])
-    def test_navigation(self, driver, nav):
-        self.base_page.login("blessrng17@gmail.com", "testpass123")
-        self.base_page.find(*basic_locators.NAVIGATION_LOCATOR[nav]).click()
+    # # Параметризованный тест на переход по навигационному полю. Правда из пула убран PRO, потому что он по кд ломается
+    # @pytest.mark.skip('skip')
+    # @pytest.mark.parametrize("nav", [(random.randint(0, 4)),
+    #                                  (random.randint(0, 4))])
+    # def test_navigation(self, driver, nav):
+    #     self.base_page.login("blessrng17@gmail.com", "testpass123")
+    #     self.base_page.find(*basic_locators.NAVIGATION_LOCATOR[nav]).click()
 
     # Тест на изменение информации
-    @pytest.mark.skip('skip')
+    #@pytest.mark.skip('skip')
     def test_change_info(self, driver):
         self.base_page.login("blessrng17@gmail.com", "testpass123")
         self.base_page.change()
