253911bd30da71c98ed2ba26153f63b847042190
ayypeegeepee <blessrng17@gmail.com>
2022-10-18 13:24:34 +0300

    new changes

--- homework2/ui/locators/basic_locators.py
+++ homework2/ui/locators/basic_locators.py
@@ -24,14 +24,15 @@ class BasePageLocators:
     PASSWORD_LOCATOR = (By.NAME, 'password')
     ENTER_LOCATOR = (By.XPATH, "//div[contains(@class, 'authForm-module-button')]")
     BASEPAGE_LOCATOR = (By.XPATH, "//div[contains(@class, 'mainPage-module-body')]")
-    DISPLAYED_SITE_LOCATOR = (By.XPATH, "//ul[contains(@class, 'instruction-module-list')]")
+    DISPLAYED_SITE_LOCATOR = (By.XPATH, "//ul[contains(@class, 'instruction-module-list')] | //div[contains(@class, 'pagination-module-pagination-')]")
 
 
 class AdPageLocators:
     NEW_AD_CAMPAIGN_LOCATOR = (By.XPATH, "//ul/a[contains(@href,'/campaign/new')]")
-    NOT_NEW_AD_CAMPAIGN_LOCATOR = (By.XPATH, "//div[contains(@class,'dashboard-module-headControlsWrapper-')]")
+    NOT_NEW_AD_CAMPAIGN_LOCATOR = (By.XPATH, "//div[contains(@class, 'pagination-module-pagination-')]")
     AD_VISIBILITY_LOCATOR = (By.XPATH, "//div[contains(@class, 'js-objectives-container')]")
     AD_WATCH_VIDEO_LOCATOR = (By.XPATH, "//div[contains(@class,'_videoviews')]")
     ENTER_URL_LOCATOR = (By.XPATH, "//div/input[contains(@class,'input-module-input')]")
+
     VISIBITY_OF_COMPAIGN_INFO_LOCATOR = (By.XPATH, "//div[contains(@data-name, 'whomTargeting')]")
 
