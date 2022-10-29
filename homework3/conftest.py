6f0ac506e17dbee6c6d76edcc6b5266bd00f5a26
ayypeegeepee <blessrng17@gmail.com>
2022-10-29 13:56:45 +0300

    start

---
+++ homework3/conftest.py
@@ -0,0 +1,62 @@
+import logging
+import shutil
+import time
+from ui.fixtures import *
+from datetime import datetime
+
+
+def pytest_addoption(parser):
+    parser.addoption("--url", default="https://target-sandbox.my.com/")
+    parser.addoption('--headless', action='store_true')
+    parser.addoption('--debug_log', action='store_true')
+
+
+@pytest.fixture(scope='session')
+def repo_root():
+    return os.path.abspath(os.path.join(__file__, os.path.pardir))
+
+
+@pytest.fixture(scope='session')
+def base_temp_dir(config):
+    base_dir = './info'
+    if os.path.exists(base_dir):
+        shutil.rmtree(base_dir)
+    return base_dir
+
+
+@pytest.fixture(scope='function')
+def temp_dir(request):
+    time = datetime.now()
+    test_dir = os.path.join(request.config.base_temp_dir, request.node.name)
+    os.makedirs(test_dir)
+    return test_dir
+
+
+@pytest.fixture(scope='session')
+def config(request):
+    url = request.config.getoption("--url")
+    headless = request.config.getoption("--headless")
+    debug_log = request.config.getoption('--debug_log')
+    return {"url": url, "headless": headless, 'debug_log': debug_log}
+
+
+@pytest.fixture(scope='function')
+def logger(temp_dir, config):
+    log_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
+    log_file = os.path.join(temp_dir, 'test.log')
+    log_level = logging.DEBUG if config['debug_log'] else logging.INFO
+
+    file_handler = logging.FileHandler(log_file, 'w')
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(log_level)
+
+    log = logging.getLogger('test')
+    log.propagate = False
+    log.setLevel(log_level)
+    log.handlers.clear()
+    log.addHandler(file_handler)
+
+    yield log
+
+    for handler in log.handlers:
+        handler.close()
---
+++ homework3/ui/pages/segment_page.py
@@ -0,0 +1,32 @@
+import selenium.webdriver.support.expected_conditions as ec
+from ui.locators import basic_locators
+from ui.pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import allure
+
+class SegmentPage(BasePage):
+    locators = basic_locators.SegmentLocators()
+
+    @allure.step("Функция change_name присваивает сегменту имя с UUID")
+    def change_name(self, segment_name):
+        action = ActionChains(self.driver)
+        actions = self.find(basic_locators.SegmentLocators.SEGMENT_NAME_PLACEHOLDER_LOCATOR)
+        action.double_click(actions).double_click(actions).send_keys(Keys.DELETE).send_keys(segment_name).perform()
+
+    @allure.step("Функция create_segment создает сегмент")
+    def create_segment(self, segment_name, timeout=None):
+        self.driver.get("https://target-sandbox.my.com/segments")
+        self.wait_and_click(basic_locators.SegmentLocators.CREATE_SEGMENT_LOCATOR)
+        self.wait_and_click(basic_locators.SegmentLocators.APPS_GAMES_LOCATOR)
+        self.wait_and_click(basic_locators.SegmentLocators.CHECKBOX_LOCATOR)
+        self.wait_and_click(basic_locators.SegmentLocators.ADD_SEGMENT_LOCATOR)
+        self.change_name(segment_name)
+        self.wait_and_click(basic_locators.SegmentLocators.CREATE_SEGMENT_LOCATOR)
+
+    @allure.step("Функция success_check проверяет создание сегмента с таким же UUID")
+    def success_check(self, segment_name, timeout=None):
+        self.wait(timeout).until(ec.element_to_be_clickable(self.locators.DISPLAYED_SEGMENTS_LOCATOR))
+        SUCCESS_LOCATOR = (By.XPATH, f"//a[@title= '{segment_name}']")
+        self.wait_presence(SUCCESS_LOCATOR, timeout=5)
