426e962b8989ef7f20c6f73a220d8534c5962984
ayypeegeepee <blessrng17@gmail.com>
2022-11-18 03:43:37 +0300

    changed hm3

--- homework3/api/client.py
+++ homework3/api/client.py
@@ -4,6 +4,11 @@ import uuid
 import json
 import os
 
+
+def repo_root():
+    return os.path.abspath(os.path.join(__file__, os.path.pardir))
+
+
 class ApiClient:
     def __init__(self):
         self.session = requests.Session()
@@ -19,88 +24,68 @@ class ApiClient:
             'failure': 'https://account.my.com/login/',
             'continue': 'https://target-sandbox.my.com/auth/mycom?state=target_login%3D1%26ignore_opener%3D1#email'
         }
-        login_session = self._request(method="POST", given_url='https://auth-ac.my.com/auth?lang=ru&nosavelogin=0',
-                                      headers=headers, data=data, location=None,
-                                      allow_redirects=True)
+        self._request(method="POST", given_url='https://auth-ac.my.com/auth?lang=ru&nosavelogin=0',
+                      headers=headers, data=data, location=None,
+                      allow_redirects=True)
 
+    def get_crsf_header(self):
         self.session.get(url='https://target-sandbox.my.com/csrf')
-        return login_session
+        csrf = self.session.cookies.get_dict()['csrftoken']
+        headers = {
+            "X-CSRFToken": f'{csrf}'
+        }
+        return headers
 
     def _request(self, given_url, method, location, headers, data, params=None, allow_redirects=False):
         url = urljoin(given_url, location)
-        response = self.session.request(method=method, url=url, headers=headers, data=data, params=params,
-                                        allow_redirects=allow_redirects)
-        code = response.status_code
+        self.session.request(method=method, url=url, headers=headers, data=data, params=params,
+                             allow_redirects=allow_redirects)
 
     def post_segment_vk(self, name):
-        dict = self.session.cookies.get_dict()
-        csrf = dict['csrftoken']
-        headers = {
-            "X-CSRFToken": f'{csrf}'
-        }
-
-        data = self.read_json(filename='post_segment_vk',name=name)
-        func = self.session.post(url='https://target-sandbox.my.com/api/v2/remarketing/segments.json', json=data,
-                                 headers=headers)
+        data = self.read_json(filename='post_segment_vk', name=name)
+        self.session.post(url='https://target-sandbox.my.com/api/v2/remarketing/segments.json', json=data,
+                          headers=self.get_crsf_header())
 
     def post_segment_games(self, name):
-        dict = self.session.cookies.get_dict()
-        csrf = dict['csrftoken']
-        headers = {
-            "X-CSRFToken": f'{csrf}'
-        }
-        data = self.read_json(filename='post_segment_games',name=name)
-        func = self.session.post(url='https://target-sandbox.my.com/api/v2/remarketing/segments.json', json=data,
-                                 headers=headers)
+        data = self.read_json(filename='post_segment_games', name=name)
+        self.session.post(url='https://target-sandbox.my.com/api/v2/remarketing/segments.json', json=data,
+                          headers=self.get_crsf_header())
 
     def delete_segment(self, name):
-        dict = self.session.cookies.get_dict()
-        csrf = dict['csrftoken']
-        headers = {
-            "X-CSRFToken": f'{csrf}'
-        }
         json = self.session.get(url='https://target-sandbox.my.com/api/v2/remarketing/segments.json').json()
         for i in range(len(json['items'])):
             if str(json['items'][i]['name']) == str(name):
-                buf = json['items'][i]['id']
-                deletion = self.session.delete(
-                    url=f'https://target-sandbox.my.com/api/v2/remarketing/segments/{buf}.json',
-                    headers=headers)
+                self.session.delete(
+                    url=f'https://target-sandbox.my.com/api/v2/remarketing/segments/{json["items"][i]["id"]}.json',
+                    headers=self.get_crsf_header())
 
     def post_campaign(self, name):
-        dict = self.session.cookies.get_dict()
-        csrf = dict['csrftoken']
-        headers = {
-            "X-CSRFToken": f'{csrf}'
-        }
-
         data = self.read_json(filename='post_campaing', name=name)
-        func = self.session.post(url='https://target-sandbox.my.com/api/v2/campaigns.json', json=data,
-                                 headers=headers)
+        data['banners'][0]['content']['video_landscape_30s']['id'] = f'{self.send_video()}'
+        self.session.post(url='https://target-sandbox.my.com/api/v2/campaigns.json', json=data,
+                          headers=self.get_crsf_header())
 
     def delete_campaign(self, name):
-        dict = self.session.cookies.get_dict()
-        csrf = dict['csrftoken']
-        headers = {
-            "X-CSRFToken": f'{csrf}'
-        }
         json = self.session.get(
             url='https://target-sandbox.my.com/api/v2/campaigns.json?fields=id%2Cname&sorting=-id&_status__in=active').json()
         for i in range(len(json['items'])):
             if str(json['items'][i]['name']) == str(name):
-                buf = json['items'][i]['id']
-                deletion = self.session.delete(url=f'https://target-sandbox.my.com/api/v2/campaigns/{buf}.json',
-                                               headers=headers)
+                self.session.delete(url=f'https://target-sandbox.my.com/api/v2/campaigns/{json["items"][i]["id"]}.json',
+                                    headers=self.get_crsf_header())
 
     def read_json(self, filename, name):
-        path = self.repo_root()
         with open(
-                f'{path}/files/{filename}.json') as f:
+                f'{repo_root()}/files/{filename}.json') as f:
             info = json.load(f)
             info['name'] = f'{name}'
-
         return info
 
-    def repo_root(self):
-        return os.path.abspath(os.path.join(__file__, os.path.pardir))
-
+    def send_video(self):
+        video = open(f'{repo_root()}/files/ad.mp4', 'rb')
+        file = {
+            "Content-Type": "multipart/form-data",
+            "file": video
+        }
+        request = self.session.post(url='https://target-sandbox.my.com/api/v2/content/video.json',
+                                    headers=self.get_crsf_header(), files=file)
+        return request.json()['id']
