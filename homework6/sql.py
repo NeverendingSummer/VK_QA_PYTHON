2d3361d837eff7a6a695ebe21f22b28c85f335b7
ayypeegeepee <blessrng17@gmail.com>
2022-12-02 15:18:52 +0300

    fix2

--- homework6/sql.py
+++ homework6/sql.py
@@ -9,7 +9,7 @@ class MysqlClient:
     def __init__(self, db_name, user, password):
         self.user = 'root'
         self.port = 3306
-        self.password = '2283221488'
+        self.password = '0000'
         self.host = '127.0.0.1'
         self.db_name = db_name
         self.connection = None
